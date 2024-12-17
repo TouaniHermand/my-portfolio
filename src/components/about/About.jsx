5a9ce73fb39e815d4c55c306a505f3ecbdc7290c
TouaniHermand <willfriedtouani@gmail.com>
2024-12-17 14:55:42 +0100

    about update

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import "./about.css";
-import ME from "../../assets/me-about.jpg";
+import ME from "../../assets/me-about.jpeg";
+import { FaAward } from "react-icons/fa";
+import { FiUsers } from "react-icons/fi";
+import { VscFolderLibrary } from "react-icons/vsc";
 
 const About = () => {
   return (
@@ -14,13 +17,37 @@ const About = () => {
             <img src={ME} alt="About Image" />
           </div>
         </div>
-        <div className="about__content">
+        <div className="about__content ">
           <div className="about__cards">
             <article className="about__card">
+              <FaAward className="about__icon" />
               <h5>Experience</h5>
               <small>1+ Years Working</small>
             </article>
+            <article className="about__card">
+              <FiUsers className="about__icon" />
+              <h5>Clients</h5>
+              <small>200+ Worldwide</small>
+            </article>
+            <article className="about__card">
+              <VscFolderLibrary className="about__icon" />
+              <h5>Projects</h5>
+              <small>10+ Projects completed</small>
+            </article>
           </div>
+          <p>
+            Je me presente Touani Hermand Willfried developpeur Web et Mobile
+            Depuis toujours, je suis passionné par les nouvelles technologies et
+            leur capacité à transformer notre quotidien. Mon objectif est de
+            rejoindre une équipe dynamique pour concevoir des solutions
+            innovantes qui répondent à des besoins concrets.Je serais ravi
+            d’échanger avec vous ! n’hésitez pas à me contacter pour discuter
+            d’un projet ou d’une collaboration.
+          </p>
+
+          <a href="#contact" className="btn btn-primary">
+            Let's Talk
+          </a>
         </div>
       </div>
     </section>
