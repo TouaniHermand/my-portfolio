49fb0133d62f4ce370f504ebaa527ca8907b8663
TouaniHermand <willfriedtouani@gmail.com>
2024-12-19 17:04:25 +0100

    services and portfolio   update

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,8 +1,74 @@
 import React from "react";
 import "./portfolio.css";
+import IMG1 from "../../assets/portfolio1.JPG";
+import IMG2 from "../../assets/portfolio2.JPG";
+import IMG3 from "../../assets/portfolio3.JPG";
+import IMG4 from "../../assets/portfolio4.JPG";
+import IMG5 from "../../assets/portfolio5.jpg";
+import IMG6 from "../../assets/portfolio6.JPG";
+
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: "full-web-site ",
+    github: "https://github.com/TouaniHermand/full-web-site ",
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: "food",
+    github: "https://github.com/TouaniHermand/food ",
+  },
+  {
+    id: 1,
+    image: IMG3,
+    title: "Moviedux ",
+    github: "https://github.com/TouaniHermand/Moviedux ",
+  },
+  {
+    id: 1,
+    image: IMG4,
+    title: "Gestion_RH  ",
+    github: "https://github.com/TouaniHermand/Gestion_RH  ",
+  },
+  {
+    id: 1,
+    image: IMG5,
+    title: "Gestion_RH  ",
+    github: "https://github.com/TouaniHermand/Gestion_RH  ",
+  },
+  {
+    id: 1,
+    image: IMG6,
+    title: "Bug-Blaster ",
+    github: "https://github.com/TouaniHermand/Bug-Blaster  ",
+  },
+];
 
 const Portfolio = () => {
-  return <section id="portfolio">Portfolio</section>;
+  return (
+    <section id="portfolio">
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+        {data.map((project) => (
+          <article className="portfolio__item" key={project.id}>
+            <div className="portfolio__item-image">
+              <img src={project.image} alt="" />
+            </div>
+            <h3>{project.title}</h3>
+            <div className="portfolio__item-cta">
+              <a href={project.github} className="btn" target="_blank">
+                Github
+              </a>
+            </div>
+          </article>
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Portfolio;
--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,8 +1,101 @@
 import React from "react";
 import "./services.css";
+import { BiCheck } from "react-icons/bi";
 
 const Services = () => {
-  return <section id="services">Services</section>;
+  return (
+    <section id="services">
+      <h5>What I Offert</h5>
+      <h2>Services</h2>
+      <div className="container services__container">
+        <article className="service">
+          <div className="service__head">
+            <h3>Web development</h3>
+          </div>
+          <ul className="service__list">
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Creation of responsive and interactive web sites</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>
+                Development of web applications using frameworks such a React js
+                and Vue js
+              </p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>
+                Creation of API's to enable integration with other applications
+              </p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Integration of database to store and retrieve data</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Optimisation of web application performance</p>
+            </li>
+          </ul>
+        </article>
+
+        <article className="service">
+          <div className="service__head">
+            <h3>Mobile development</h3>
+          </div>
+          <ul className="service__list">
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Creation of mobile applications for Android ans iOS</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>
+                Development of hybrid applications using frameworks such as
+                React Native
+              </p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>
+                Creation of API's to enable integration with other applications
+              </p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Creation of intuitive and responsive User interfaces</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Optimisation of mobile application performance</p>
+            </li>
+          </ul>
+        </article>
+
+        <article className="service">
+          <div className="service__head">
+            <h3>Additional Sevices</h3>
+          </div>
+          <ul className="service__list">
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Design and prototyping</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Maintenance and updates of existing applications</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Client training and support for application</p>
+            </li>
+          </ul>
+        </article>
+      </div>
+    </section>
+  );
 };
 
 export default Services;
