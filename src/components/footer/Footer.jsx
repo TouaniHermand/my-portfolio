a4ae94d15cdeb7b69de1281c4dd1db0ba00bb3a7
TouaniHermand <willfriedtouani@gmail.com>
2024-12-24 01:06:41 +0100

    footer update

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,56 @@
 import React from "react";
-import './footer.css'
+import "./footer.css";
+import { FaFacebook } from "react-icons/fa";
+import { FiInstagram } from "react-icons/fi";
 
-const Footer = ()=>{
-    return (
-        <div>Footer</div>
-    )
-}
+const Footer = () => {
+  return (
+    <footer>
+      <a href="#" className="footer__logo">
+        Touani Hermand
+      </a>
 
-export default Footer
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#services">Services</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#testimonials">Testimonials</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://www.facebook.com/willfried.touani">
+          <FaFacebook />
+        </a>
+        <a href="https://www.instagram.com/willfried93/">
+          <FiInstagram />
+        </a>
+        <a href="https://www.instagram.com/willfried93/">
+          <FiInstagram />
+        </a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; portfolio. All rights reserved.</small>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
